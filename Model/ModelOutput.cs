c70005d767a803904579d72aca1a59297c622a24
Slimcent <centpresley@gmail.com>
2023-05-23 23:08:06 +0100

    Prediction method.

---
+++ Model/ModelOutput.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace CreditRiskCustomerPrediction.Model
+{
+    public class ModelOutput
+    {
+        [ColumnName("PredictedLabel")]
+        public string Prediction { get; set; }
+    }
+}
